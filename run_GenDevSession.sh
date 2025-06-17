1361f287f6efa0184b5c0d5212acbbb0e1a5db06
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2025-06-17 12:31:50 -0600

    Create run_GenDevSession.sh

---
+++ run_GenDevSession.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+date=$(date '+%Y-%m-%d__%H%M')
+echo "$date :: BEGIN JOB."
+source /Projects/scale_metrics/vfx/bin/activate #launch the venv version
+#pip list         #list the items installed in VENV
+#echo "$date :: pip_list has been omitted! not showing list.."
+python_executable="/Projects/scale_metrics/vfx/bin/python"
+bash_executable="/bin/bash"
+cd "/Projects/scale_metrics"
+echo "$date :: Currently within directory - $(pwd)"
+
+# Path to session file
+SESSION_FILE="session/devcall_sessionLogin.p"
+# Check if session file exists
+if [ -f "$SESSION_FILE" ]; then
+    # Get the last modified time of the session file
+    last_mod_time=$(stat -c %Y "$SESSION_FILE")
+    current_time=$(date +%s)
+    age=$((current_time - last_mod_time))
+    
+    if [ "$age" -ge 43200 ]; then
+        date=$(date '+%Y-%m-%d__%H%M')
+        echo "$date :: Session file is older than 12 hours, killing this old session."
+        $python_executable kill_sessionID4Dev.py
+        date=$(date '+%Y-%m-%d__%H%M')
+        echo "$date :: Regenerating session..."
+        $python_executable gen_sessionID4Dev.py
+        date=$(date '+%Y-%m-%d__%H%M')
+        echo "$date :: END JOB."
+    else
+        echo "$date :: Login Found, no action is needed! Using existing session file from $last_mod_time / $current_time"
+        date=$(date '+%Y-%m-%d__%H%M')
+        echo "$date :: END JOB."
+    fi
+else
+    echo "$date :: Session file does not exist, generating a new session."
+    touch $SESSION_FILE
+    $python_executable gen_sessionID4Dev.py
+    date=$(date '+%Y-%m-%d__%H%M')
+    echo "$date :: END JOB."
+
+fi
